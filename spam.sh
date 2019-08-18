0cf5f8a86d64cedc807e3b7fb5180c681477e0c6
ZCYBORG-404 <51997622+ZCYBORG-404@users.noreply.github.com>
2019-08-18 16:45:26 +0800

    Add files via upload

---
+++ spam.sh
@@ -0,0 +1,44 @@
+clear
+
+termux-setup-storage
+
+echo "SPAM DATA INTERNAL" | lolcat
+sleep 1
+echo ""
+read -p "Yakin Mau Lanjut(y/n):" p
+case $p in
+
+y) clear
+echo "ok lanjut"
+sleep 1
+
+;;
+
+n) clear
+echo "wait...." | lolcat
+clear
+echo "EXIT...." | lolcat
+exit
+
+;;
+esac
+
+echo "======================"
+echo "1.spam gambar"
+echo "======================"
+echo "2.INSTALL BAHAN"
+echo "======================"
+read -p "Masukan No:" pila
+case $pila in
+1) clear
+read -p "Masukan no Korban:" haja
+echo "INGAT SPAM INI UNLIMITED GAYN!!"
+sleep 3
+python2 pepek.py
+
+;;
+
+2) python2  yeahno5.py
+;;
+
+esac
